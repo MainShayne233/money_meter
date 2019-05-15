343f980ba0d165333b35d2ea67488b80b5c892db
MainShayne233 <shaynetremblay@gmail.com>
2019-05-15 16:53:59 -0400

    Use pyqt5

--- src/main.py
+++ src/main.py
@@ -1,16 +1,27 @@
 import sys
-from PyQt4 import QtGui
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtCore import QSize
 
-def run():
-   app = QtGui.QApplication(sys.argv)
-   window = QtGui.QWidget()
-   label = QtGui.QLabel(window)
-   label.setText("Money Meter!!!")
-   window.setGeometry(100,100,200,50)
-   label.move(50, 20)
-   window.setWindowTitle("Money Meter")
-   window.show()
-   sys.exit(app.exec_())
+class HelloWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
 
-if __name__ == '__main__':
-   run()
+        self.setMinimumSize(QSize(640, 480))
+        self.setWindowTitle("Hello world")
+
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        gridLayout = QGridLayout(self)
+        centralWidget.setLayout(gridLayout)
+
+        title = QLabel("Hello World from PyQt", self)
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        gridLayout.addWidget(title, 0, 0)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = HelloWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )

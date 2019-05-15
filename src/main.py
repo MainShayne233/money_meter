02474b780a199603fa325b3b2d21fb5880297991
MainShayne233 <shaynetremblay@gmail.com>
2019-05-15 10:06:21 -0400

    Hello world PyQT

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+import sys
+from PyQt4 import QtGui
+
+def run():
+   app = QtGui.QApplication(sys.argv)
+   window = QtGui.QWidget()
+   label = QtGui.QLabel(window)
+   label.setText("Money Meter!!!")
+   window.setGeometry(100,100,200,50)
+   label.move(50, 20)
+   window.setWindowTitle("Money Meter")
+   window.show()
+   sys.exit(app.exec_())
+
+if __name__ == '__main__':
+   run()
